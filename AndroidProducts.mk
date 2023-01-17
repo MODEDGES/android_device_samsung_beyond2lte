2dfdc11952f5e145bf1d250758b915debccd5273
MODEDGES <106741581+MODEDGES@users.noreply.github.com>
2023-01-17 17:57:28 +0000

    Changed from Lineage To Spark
    PRODUCT_MAKEFILES := \
        $(LOCAL_DIR)/spark_beyond2lte.mk
    spark_beyond2lte-eng \
        spark_beyond2lte-user \
        spark_beyond2lte-userdebug

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond2lte.mk
+    $(LOCAL_DIR)/spark_beyond2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond2lte-eng \
-    lineage_beyond2lte-user \
-    lineage_beyond2lte-userdebug
+    spark_beyond2lte-eng \
+    spark_beyond2lte-user \
+    spark_beyond2lte-userdebug
