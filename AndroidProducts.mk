c61fe595db0c794cdf76f130551ced5477a54140
MODEDGES <106741581+MODEDGES@users.noreply.github.com>
2023-04-27 22:41:40 +0100

    Update AndroidProducts.mk
    Added common lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond2lte.mk
+    $(LOCAL_DIR)/droidx_beyond2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond2lte-eng \
-    lineage_beyond2lte-user \
-    lineage_beyond2lte-userdebug
+    droidx_beyond2lte-eng \
+    droidx_beyond2lte-user \
+    droidx_beyond2lte-userdebug
