58bacbfd5d817953dcf4a986266f511a813bf868
MODEDGES <106741581+MODEDGES@users.noreply.github.com>
2023-08-07 11:55:30 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond2lte.mk
+    $(LOCAL_DIR)/superior_beyond2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond2lte-eng \
-    lineage_beyond2lte-user \
-    lineage_beyond2lte-userdebug
+    superior_beyond2lte-eng \
+    superior_beyond2lte-user \
+    superior_beyond2lte-userdebug
